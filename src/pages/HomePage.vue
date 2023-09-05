ba09b9a0030c171e0b31a3568bb5e3c46f424617
SteCard <stefanocardillo14@gmail.com>
2023-09-05 02:00:04 +0200

    Homepage con testo completa

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -12,6 +12,22 @@ export default {
             <div class="col-12 py-5">
                 <h1 class="text-center">Homepage</h1>
             </div>
+
+            <!-- HOME PAGE DESCRIPTION -->
+            <div class="col-12 py-5">
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati, saepe eveniet! Deserunt mollitia atque voluptate repudiandae ratione est deleniti obcaecati aliquid facere, pariatur illum blanditiis magnam, perferendis voluptatum nam? Officiis?
+                    <br><br>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam cum perspiciatis repellendus voluptatem, magni consectetur quasi sint sunt, possimus animi vel adipisci dicta voluptate eligendi. Aperiam molestias repellendus similique dicta!
+                    <br><br>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse fugit sequi fuga porro, illo ut in nobis beatae voluptatibus quaerat ex vel adipisci excepturi aliquid consectetur maiores tempore, at corporis!
+                    <br><br>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ipsa recusandae illo alias nobis distinctio totam! Modi sed dignissimos odit iste dicta voluptatibus, blanditiis, animi cumque facere excepturi quidem vitae!
+                    <br><br>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea delectus quos quasi perspiciatis voluptas, voluptatum fuga asperiores totam amet ab autem quam dignissimos beatae! Rerum possimus veniam nemo et similique?
+                </p>
+            </div>
+            
         </div>
     </div>
 </template>
